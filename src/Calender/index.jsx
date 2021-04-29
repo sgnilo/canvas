c49a6f9c6d39fef3c61bd29def66ac4b8f9a57cd
sgnilo <xujiangpingtdb@icloud.com>
2021-04-29 16:56:44 +0800

    调整布局

--- src/Calender/index.jsx
+++ src/Calender/index.jsx
@@ -3,30 +3,31 @@ import CircleTable from '../CircleTable/index.jsx';
 import './Calender.less';
 import Ajax from '../util/request';
 
-const colorMap = [{
-    color: 'rgb(81, 107, 145)',
-    activeColor: 'rgba(81, 107, 145, 0.8)'
-},{
-    color: 'rgb(89, 196, 230)',
-    activeColor: 'rgba(89, 196, 230, .8)'
-},{
-    color: 'rgb(237, 175, 218)',
-    activeColor: 'rgba(237, 175, 218, .8)'
-},{
-    color: 'rgb(147, 183, 227)',
-    activeColor: 'rgba(147, 183, 227, .8)'
-},{
-    color: 'rgb(165, 231, 240)',
-    activeColor: 'rgba(165, 231, 240, .8)'
-},{
-    color: 'rgb(203, 176, 227)',
-    activeColor: 'rgba(203, 176, 227, .8)'
-}
+const colorMap = [
+    {
+        color: 'rgb(81, 107, 145)',
+        activeColor: 'rgba(81, 107, 145, 0.8)'
+    },{
+        color: 'rgb(89, 196, 230)',
+        activeColor: 'rgba(89, 196, 230, .8)'
+    },{
+        color: 'rgb(237, 175, 218)',
+        activeColor: 'rgba(237, 175, 218, .8)'
+    },{
+        color: 'rgb(147, 183, 227)',
+        activeColor: 'rgba(147, 183, 227, .8)'
+    },{
+        color: 'rgb(165, 231, 240)',
+        activeColor: 'rgba(165, 231, 240, .8)'
+    },{
+        color: 'rgb(203, 176, 227)',
+        activeColor: 'rgba(203, 176, 227, .8)'
+    }
 ]
 
 const Calender = props => {
 
-    const {dates, onchange} = props;
+    const {onChange} = props;
     const [activeDay, setActiveDay] = useState(0);
     const [dates, setDates] = useState([]);
 
@@ -54,18 +55,19 @@ const Calender = props => {
 
     const preProcess = list => {
         return list.map(item => {
-            const {time, ratio_rb} = item;
-            return {date: time, info: tableProcess(ratio_rb)};
+            const {time, ratio_rb, ratio_tb} = item;
+            return {date: time, send: tableProcess(ratio_rb), recv: tableProcess(ratio_tb)};
         });
     };
 
     useEffect(() => {
-        console.log(dates[activeDay], dates[activeDay]);
         Ajax.get({url: 'http://192.168.8.8:8010/all_time'}).then(res => {
             const result = JSON.parse(res);
-            setDates(preProcess(result));
+            const list = preProcess(result);
+            setDates(list);
+            onChange && onChange(list[0]);
         }).catch(err => console.error(err));
-    }, [activeDay]);
+    }, []);
 
     return <div className="calender">
         <div className="dates-area">
--- src/app.jsx
+++ src/app.jsx
@@ -9,101 +9,29 @@ import CircleTable from './CircleTable/index.jsx';
 
 function App(props) {
 
-    const [dates, setDates] = useState([]);
     const [isTcp, setTcpState] = useState(true);
     const [sendData, setSendData] = useState(null);
     const [recvData, setRecvData] = useState(null);
-
-
-    
-    useEffect(() => {
-        // Ajax.jsonp({
-        //     url: '/api/wall'
-        // }).then(res => {
-        // });
-        
-        
-    }, []);
-    
-
-    const myDate = [
-        {
-            date: '4月21'
-        },
-        {
-            date: '4月22'
-        },
-        {
-            date: '4月23'
-        },
-        {
-            date: '4月24'
-        },
-        {
-            date: '4月25'
-        },
-        {
-            date: '4月26'
-        },
-        {
-            date: '4月27'
-        },
-        {
-            date: '4月28'
-        },
-        {
-            date: '4月29'
-        },
-        {
-            date: '4月30'
-        },
-        {
-            date: '5月1日'
-        }
-    ]
-
-    useEffect(() => {
-        const tempDate = myDate.map((item, index) => {
-            let info = [
-                {percent: 0.1, color: 'rgb(0, 255, 255)', name: 'a', activeColor: 'rgba(0, 255, 255, 0.6)'},
-                {percent: 0.15, color: 'rgb(0, 211, 255)', name: 'b', activeColor: 'rgba(0, 211, 255, 0.6)'},
-                {percent: 0.2, color: 'rgb(0, 177, 255)', name: 'c', activeColor: 'rgba(0, 177, 255, 0.6)'},
-                {percent: 0.25, color: 'rgb(0, 144, 255)', name: 'd', activeColor: 'rgba(0, 144, 255, 0.6)'},
-                {percent: 0.3, color: 'rgb(0, 100, 255)', name: 'e', activeColor: 'rgba(0, 100, 255, 0.6)'}
-            ];
-            if (index % 2) {
-                info = [
-                    {percent: 0.4, color: 'rgb(0, 255, 255)', name: 'a', activeColor: 'rgba(0, 255, 255, 0.6)'},
-                    {percent: 0.05, color: 'rgb(0, 211, 255)', name: 'b', activeColor: 'rgba(0, 211, 255, 0.6)'},
-                    {percent: 0.15, color: 'rgb(0, 177, 255)', name: 'c', activeColor: 'rgba(0, 177, 255, 0.6)'},
-                    {percent: 0.2, color: 'rgb(0, 144, 255)', name: 'd', activeColor: 'rgba(0, 144, 255, 0.6)'},
-                    {percent: 0.2, color: 'rgb(0, 100, 255)', name: 'e', activeColor: 'rgba(0, 100, 255, 0.6)'}
-                ]
-            }
-            return {...item, info}
-            
-        });
-        setDates(tempDate);
-    }, []);
+    const [date, setDate] = useState('');
 
     const chooseOne = status => {
         setTcpState(status);
     }
 
     const onChange = data => {
-        console.log(data);
-        setSendData(data);
-        setRecvData(data);
+        const {date: time, ratio_rb, ratio_tb} = data;
+        setDate(time);
+        setSendData(ratio_tb);
+        setRecvData(ratio_rb);
     }
 
-    
     return <div className="all-wrapper">
         <div className="choose-box">
             <div className={`choose-item${isTcp ? ' active-item' : ''}`} onClick={() => chooseOne(true)}>TCP</div>
             <div className={`choose-item${isTcp ? '' : ' active-item'}`} onClick={() => chooseOne(false)}>UDP</div>
         </div>
         <div className="left-block">
-            <Calender dates={dates} onChange={onChange} />
+            <Calender onChange={onChange} />
             <div className="network-wrapper">
                 {isTcp ? <Tcp /> : <Udp />}
             </div>
@@ -111,9 +39,12 @@ function App(props) {
         <div className="right-block">
             {sendData && <div className="every-circle">
                 <CircleTable data={sendData} />
-                <p className="table-name">TCP流量分析扇形图1</p>
+                <p className="table-name">{date}TCP发送端流量分析扇形图</p>
             </div>}
-
+            {recvData && <div className="every-circle">
+                <CircleTable data={sendData} />
+                <p className="table-name">{date}TCP接收端流量分析扇形图</p>
+            </div>}
         </div>
     </div>
 };
